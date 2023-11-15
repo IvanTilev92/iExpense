2dfa43c1d9bf196bcfdd220bbf472284a3c91db6
IvanTilev <iivantilev@gmail.com>
2023-11-15 19:50:45 +0200

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Ivan Tilev on 15.11.23.
+//
+
+import SwiftUI
+
+// Struct that represents a single expence
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+// Storing ExpenseItems in a array
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
+
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add expense", systemImage: "plus") {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+                }
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ivan Tilev on 15.11.23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
